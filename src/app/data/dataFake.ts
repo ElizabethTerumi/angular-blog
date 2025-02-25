dea68554336e61007e145e3b30e6044270cf44d7
Elizabeth Terumi <etifel16@gmail.com>
2025-02-25 08:20:23 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake=[
+    {
+        "id":1,
+        "title": "Cidade São Paulo",
+        "description":"olayyyyyyyyyyyyyyy",
+        "photoCover":"https://cdn.pixabay.com/photo/2016/11/20/08/34/buildings-1842205_1280.jpg"    },
+    {
+        "id":2,
+        
+        "title": "Top 01 - Ibirapuera",
+        "description":" laggooooollaggoooooooooooooooooooooooooa",
+        "photoCover":"https://www.melhoresdestinos.com.br/wp-content/uploads/2019/02/passagens-aereas-sao-paulo-capa2019-04-820x430.jpg"},
+    
+    {
+        "id":3,
+        
+        "title": "Paulista",
+        "description":"domingos centro",
+        "photoCover":"https://cdn.pixabay.com/photo/2016/11/20/08/34/buildings-1842205_1280.jpg"},
+    
+    {
+        "id":4,
+        
+        "title": "Liberdade",
+        "description":"restauntessss culinario",
+        "photoCover":"https://cdn.pixabay.com/photo/2016/11/20/08/34/buildings-1842205_1280.jpg"},
+    
+]
